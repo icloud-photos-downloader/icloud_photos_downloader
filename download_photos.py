971d4d9805c867e7125ad192946f315128309c2a
Nathan Broadbent <ndbroadbent@users.noreply.github.com>
2017-02-25 23:22:30 +0700

    Fix some pylint errors

--- download_photos.py
+++ download_photos.py
@@ -18,22 +18,23 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.command(context_settings=CONTEXT_SETTINGS, options_metavar='<options>')
 @click.argument('directory', type=click.Path(exists=True), metavar='<directory>')
 @click.option('--username',
-    help='Your iCloud username or email address',
-    metavar='<username>',
-    prompt='iCloud username/email')
+              help='Your iCloud username or email address',
+              metavar='<username>',
+              prompt='iCloud username/email')
 @click.option('--password',
-    help='Your iCloud password (leave blank if stored in keyring)',
-    metavar='<password>')
+              help='Your iCloud password (leave blank if stored in keyring)',
+              metavar='<password>')
 @click.option('--size',
-    help='Image size to download (default: original)',
-    type=click.Choice(['original', 'medium', 'thumb']),
-    default='original')
+              help='Image size to download (default: original)',
+              type=click.Choice(['original', 'medium', 'thumb']),
+              default='original')
 @click.option('--download-videos',
-    help='Download both videos and photos (default: only download photos)',
-    is_flag=True)
+              help='Download both videos and photos (default: only download photos)',
+              is_flag=True)
 @click.option('--force-size',
-    help='Only download the requested size (default: download original if requested size is not available)',
-    is_flag=True)
+              help='Only download the requested size ' + \
+                   '(default: download original if requested size is not available)',
+              is_flag=True)
 
 
 def download(directory, username, password, size, download_videos, force_size):
@@ -42,27 +43,30 @@ def download(directory, username, password, size, download_videos, force_size):
     icloud = authenticate(username, password)
 
     # From: https://github.com/torarnv/pyicloud/tree/photos-update
-    print("Updating photos...")
+    print "Updating photos..."
     icloud.photos.update()
 
-    print("Looking up all photos...")
+    print "Looking up all photos..."
     all_photos = icloud.photos.all
     photos_count = len(all_photos.photos)
 
     directory = directory.rstrip('/')
 
     if download_videos:
-        print(("Downloading %d %s photos and videos to %s/ ..." % (photos_count, size, directory)))
+        print "Downloading %d %s photos and videos to %s/ ..." % (photos_count, size, directory)
     else:
-        print(("Downloading %d %s photos to %s/ ..." % (photos_count, size, directory)))
+        print "Downloading %d %s photos to %s/ ..." % (photos_count, size, directory)
 
-    pbar = tqdm(all_photos, total=photos_count)
+    progress_bar = tqdm(all_photos, total=photos_count)
 
-    for photo in pbar:
-        for i in range(MAX_RETRIES):
+    for photo in progress_bar:
+        for _ in range(MAX_RETRIES):
             try:
-                if not download_videos and not photo.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-                    pbar.set_description("Skipping %s, only downloading photos." % photo.filename)
+                if not download_videos \
+                    and not photo.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+
+                    progress_bar.set_description(
+                      "Skipping %s, only downloading photos." % photo.filename)
                     continue
 
                 created_date = parse(photo.created)
@@ -72,7 +76,7 @@ def download(directory, username, password, size, download_videos, force_size):
                 if not os.path.exists(download_dir):
                     os.makedirs(download_dir)
 
-                download_photo(photo, size, force_size, download_dir, pbar)
+                download_photo(photo, size, force_size, download_dir, progress_bar)
                 break
 
             except (requests.exceptions.ConnectionError, socket.timeout):
@@ -87,7 +91,7 @@ def download(directory, username, password, size, download_videos, force_size):
 
 
 def authenticate(username, password):
-    print("Signing in...")
+    print "Signing in..."
 
     if password:
       icloud = PyiCloudService(username, password)
@@ -95,22 +99,22 @@ def authenticate(username, password):
       icloud = PyiCloudService(username)
 
     if icloud.requires_2fa:
-        print("Two-factor authentication required. Your trusted devices are:")
+        print "Two-factor authentication required. Your trusted devices are:"
 
         devices = icloud.trusted_devices
         for i, device in enumerate(devices):
-            print("  %s: %s" % (i, device.get('deviceName',
-                "SMS to %s" % device.get('phoneNumber'))))
+            print "  %s: %s" % (i, device.get('deviceName',
+                "SMS to %s" % device.get('phoneNumber')))
 
         device = click.prompt('Which device would you like to use?', default=0)
         device = devices[device]
         if not icloud.send_verification_code(device):
-            print("Failed to send verification code")
+            print "Failed to send verification code"
             sys.exit(1)
 
         code = click.prompt('Please enter validation code')
         if not icloud.validate_verification_code(device, code):
-            print("Failed to verify verification code")
+            print "Failed to verify verification code"
             sys.exit(1)
 
     return icloud
@@ -124,41 +128,50 @@ def truncate_middle(s, n):
     if n_2 < 1: n_2 = 1
     return '{0}...{1}'.format(s[:n_1], s[-n_2:])
 
-def download_photo(photo, size, force_size, download_dir, pbar):
-    # Strip any non-ascii characters.
-    filename_with_size = photo.filename.encode('utf-8').decode('ascii','ignore').replace('.', '-%s.' % size)
-    download_path = '/'.join((download_dir, filename_with_size))
+def filename_with_size(photo, size):
+    return photo.filename.encode('utf-8') \
+        .decode('ascii', 'ignore').replace('.', '-%s.' % size)
 
-    truncated_filename = truncate_middle(filename_with_size, 24)
+def download_photo(photo, size, force_size, download_dir, progress_bar):
+    # Strip any non-ascii characters.
+    filename = filename_with_size(photo, size)
+    download_path = '/'.join((download_dir, filename))
+
+    truncated_filename = truncate_middle(filename, 24)
     truncated_path = truncate_middle(download_path, 72)
 
     if os.path.isfile(download_path):
-        pbar.set_description("%s already exists." % truncated_path)
+        progress_bar.set_description("%s already exists." % truncated_path)
         return
 
     # Fall back to original if requested size is not available
     if size not in photo.versions and not force_size and size != 'original':
-        download_photo(photo, 'original', True, download_dir, pbar)
+        download_photo(photo, 'original', True, download_dir, progress_bar)
         return
 
-    pbar.set_description("Downloading %s to %s" % (truncated_filename, truncated_path))
+    progress_bar.set_description("Downloading %s to %s" % (truncated_filename, truncated_path))
 
-    for i in range(MAX_RETRIES):
+    for _ in range(MAX_RETRIES):
         try:
-            download = photo.download(size)
+            download_url = photo.download(size)
 
-            if download:
+            if download_url:
                 with open(download_path, 'wb') as file:
-                    for chunk in download.iter_content(chunk_size=1024):
+                    for chunk in download_url.iter_content(chunk_size=1024):
                         if chunk:
                             file.write(chunk)
-            else:
-                tqdm.write("Could not find URL to download %s for size %s!" % (photo.filename, size))
+                break
 
-            return
+            else:
+                tqdm.write(
+                    "Could not find URL to download %s for size %s!" %
+                    (photo.filename, size))
+
 
         except (requests.exceptions.ConnectionError, socket.timeout):
-            tqdm.write('%s download failed, retrying after %d seconds...' % (photo.filename, WAIT_SECONDS))
+            tqdm.write(
+                '%s download failed, retrying after %d seconds...' %
+                (photo.filename, WAIT_SECONDS))
             time.sleep(WAIT_SECONDS)
     else:
         tqdm.write("Could not download %s! Maybe try again later." % photo.filename)
