57d80b00fa7130cb8742790293c58e543b26b7a5
Nathan Broadbent <git@ndbroadbent.com>
2018-07-31 04:51:13 +0700

    Fix inconsistent use of tabs and spaces

--- icloudpd/notifications.py
+++ icloudpd/notifications.py
@@ -6,27 +6,27 @@ def send_two_step_expired_notification(smtp_email, smtp_password, \
     smtp_host, smtp_port, smtp_no_tls, to_addr):
     if not to_addr:
         to_addr = smtp_email
-	logger = logging.getLogger('icloudpd')
-	logger.info("Sending 'two-step expired' notification via email...")
-	smtp = SMTP()
-	smtp.set_debuglevel(0)
-	smtp.connect(smtp_host, smtp_port)
-	if not smtp_no_tls:
-		smtp.starttls()
-	smtp.login(smtp_email, smtp_password)
+    logger = logging.getLogger('icloudpd')
+    logger.info("Sending 'two-step expired' notification via email...")
+    smtp = SMTP()
+    smtp.set_debuglevel(0)
+    smtp.connect(smtp_host, smtp_port)
+    if not smtp_no_tls:
+        smtp.starttls()
+    smtp.login(smtp_email, smtp_password)
 
-	subj = "icloud_photos_downloader: Two step authentication has expired"
-	date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+    subj = "icloud_photos_downloader: Two step authentication has expired"
+    date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
 
-	message_text = """Hello,
+    message_text = """Hello,
 
-Two-step authentication has expired for the icloud_photos_downloader script.
-Please log in to your server and run the script manually to update two-step authentication.
-"""
+    Two-step authentication has expired for the icloud_photos_downloader script.
+    Please log in to your server and run the script manually to update two-step authentication.
+    """
 
-	from_addr = "iCloud Photos Downloader <" + smtp_email + ">"
-	msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (
-	from_addr, to_addr, subj, date, message_text)
+    from_addr = "iCloud Photos Downloader <" + smtp_email + ">"
+    msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (
+    from_addr, to_addr, subj, date, message_text)
 
-	smtp.sendmail(smtp_email, to_addr, msg)
-	smtp.quit()
+    smtp.sendmail(smtp_email, to_addr, msg)
+    smtp.quit()
