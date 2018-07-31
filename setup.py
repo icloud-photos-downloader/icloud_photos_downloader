33d1f2831df46b075069e1f7224111f04a5884c2
Nathan Broadbent <git@ndbroadbent.com>
2018-08-01 05:08:56 +0700

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='icloudpd',
-    version='1.0.3',
+    version='1.0.4',
     url='https://github.com/ndbroadbent/icloud_photos_downloader',
     description=(
         'icloudpd is a command-line tool to download photos and videos from iCloud.'
