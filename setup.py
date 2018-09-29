387f453e3fec807002ccbb3263c7dc0b8738f61a
Nathan Broadbent <git@ndbroadbent.com>
2018-09-30 03:56:52 +0700

    bump version to 1.3.3

--- setup.py
+++ setup.py
@@ -1,31 +1,27 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     required = f.read().splitlines()
 
 setup(
-    name='icloudpd',
-    version='1.3.2',
-    url='https://github.com/ndbroadbent/icloud_photos_downloader',
+    name="icloudpd",
+    version="1.3.3",
+    url="https://github.com/ndbroadbent/icloud_photos_downloader",
     description=(
-        'icloudpd is a command-line tool to download photos and videos from iCloud.'
+        "icloudpd is a command-line tool to download photos and videos from iCloud."
     ),
-    maintainer='Nathan Broadbent',
-    maintainer_email='icloudpd@ndbroadbent.com',
-    license='MIT',
+    maintainer="Nathan Broadbent",
+    maintainer_email="icloudpd@ndbroadbent.com",
+    license="MIT",
     packages=find_packages(),
     install_requires=required,
     classifiers=[
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
-    entry_points={
-        'console_scripts': [
-            'icloudpd = icloudpd.base:main'
-        ]
-    },
+    entry_points={"console_scripts": ["icloudpd = icloudpd.base:main"]},
 )
