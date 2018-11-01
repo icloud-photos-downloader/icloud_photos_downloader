0daec2af6048b5653a936a3752fa70c60b7cb9eb
Chris Child <thechild@gmail.com>
2018-11-01 11:22:29 -0700

    PEP

--- icloudpd/base.py
+++ icloudpd/base.py
@@ -29,6 +29,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
+
 @click.command(context_settings=CONTEXT_SETTINGS, options_metavar="<options>")
 @click.argument(
     "directory",
