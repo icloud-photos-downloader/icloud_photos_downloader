131d5a0218d8b42c56c97ab2da971bf647aa0210
Jörg Menke <joerg.menke@gmail.com>
2019-04-18 20:25:46 +0200

    added first unit-test for --listing-albums

---
+++ tests/test_listing_albums.py
@@ -0,0 +1,95 @@
+from unittest import TestCase
+from vcr import VCR
+import os
+import shutil
+import click
+from click.testing import CliRunner
+import json
+import mock
+from icloudpd.base import main
+from tests.helpers.print_result_exception import print_result_exception
+
+vcr = VCR(decode_compressed_response=True)
+
+class ListingAlbumsTestCase(TestCase):
+
+    def test_listing_albums(self):
+        if os.path.exists("tests/fixtures/Photos"):
+            shutil.rmtree("tests/fixtures/Photos")
+        os.makedirs("tests/fixtures/Photos")
+
+        # Note - This test uses the same cassette as test_download_photos.py
+        with vcr.use_cassette("tests/vcr_cassettes/listing_photos.yml"):
+            # Pass fixed client ID via environment variable
+            os.environ["CLIENT_ID"] = "DE309E26-942E-11E8-92F5-14109FE0B321"
+            runner = CliRunner()
+            result = runner.invoke(
+                main,
+                [
+                    "--username",
+                    "jdoe@gmail.com",
+                    "--password",
+                    "password1",
+                    "--list-albums",
+                    "--no-progress-bar",
+                    "-d",
+                    "tests/fixtures/Photos",
+                ],
+            )
+
+            print_result_exception(result)
+            albums = result.output.splitlines()
+
+            self.assertEqual(len(albums), 41)
+            self.assertEqual(
+                "All Photos", albums[1]
+            )
+            self.assertEqual(
+                "Time-lapse", albums[2]
+            )
+            self.assertEqual(
+                "WhatsApp", albums[40]
+            )
+
+            assert result.exit_code == 0
+'''
+    def test_selecting_album(self):
+        if os.path.exists("tests/fixtures/Photos"):
+            shutil.rmtree("tests/fixtures/Photos")
+        os.makedirs("tests/fixtures/Photos")
+
+        # Note - This test uses the same cassette as test_download_photos.py
+        with vcr.use_cassette("tests/vcr_cassettes/listing_photos.yml"):
+            # Pass fixed client ID via environment variable
+            os.environ["CLIENT_ID"] = "DE309E26-942E-11E8-92F5-14109FE0B321"
+            runner = CliRunner()
+            result = runner.invoke(
+                main,
+                [
+                    "--username",
+                    "jdoe@gmail.com",
+                    "--password",
+                    "password1",
+                    "--album",
+                    "Koub Cafe",
+                    "--no-progress-bar",
+                    "--only-print-filenames",
+                    "-d",
+                    "tests/fixtures/Photos",
+                ],
+            )
+
+            print_result_exception(result)
+            files = result.output.splitlines()
+
+            #print(files)
+            #print(files[0])
+            #print(files[2])
+
+            self.assertEqual(len(files), 177)
+            self.assertEqual(
+                "tests/fixtures/Photos/2018/07/31/IMG_7409.JPG", files[0]
+            )
+
+            assert result.exit_code == 0
+            '''
