62c224889cf8cdede5bbceb208cb4306740eeae1
Marcell Kovács <me@marcellkovacs.com>
2023-01-18 03:39:59 +0000

    fix typos and grammar (#498)
    Co-authored-by: Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>

--- icloudpd/download.py
+++ icloudpd/download.py
@@ -71,14 +71,14 @@ def download_media(icloud, photo, download_path, size):
                     "Session error, re-authenticating...",
                     logging.ERROR)
                 if retries > 0:
-                    # If the first reauthentication attempt failed,
+                    # If the first re-authentication attempt failed,
                     # start waiting a few seconds before retrying in case
                     # there are some issues with the Apple servers
                     time.sleep(constants.WAIT_SECONDS)
 
                 icloud.authenticate()
             else:
-                # you end up here when p.e. throttleing by Apple happens
+                # you end up here when p.e. throttling by Apple happens
                 wait_time = (retries + 1) * constants.WAIT_SECONDS
                 logger.tqdm_write(
                     "Error downloading %s, retrying after %d seconds..."
