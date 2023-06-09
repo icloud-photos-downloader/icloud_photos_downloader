5c9e3dd7851328e802b4669b552231257aeaea91
Andrey Nikiforov <andy.nikiforov@gmail.com>
2023-06-09 08:43:55 -0700

    set up npm pkg for platforms

---
+++ npm/icloudpd/bin/icloudpd.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+"use strict";
+var os = require("os");
+var platformKey = `${process.platform} ${os.arch()} ${os.endianness()}`;
+var knownPlatforms = {
+    "linux x64 LE": {
+        "pkgName": "@icloudpd/linux-x64",
+        "subPath": "bin/icloudpd" 
+    },
+    "darwin x64 LE": {
+        "pkgName": "@icloudpd/darwin-x64",
+        "subPath": "bin/icloudpd" 
+    },
+    "win32 x64 LE": {
+        "pkgName": "@icloudpd/win32-x64",
+        "subPath": "bin/icloudpd.exe" 
+    }
+};
+if (platformKey in knownPlatforms) {
+    var { pkgName, subPath } = knownPlatforms[platformKey];
+    var binPath = require.resolve(`${pkgName}/${subPath}`);
+    require("child_process").execFileSync(binPath, process.argv.slice(2), { stdio: "inherit" });
+} else {
+    throw new Error(`Unsupported platform: ${platformKey}`);
+}
