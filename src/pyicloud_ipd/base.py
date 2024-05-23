5f5df6901a6a43d4b2cfd3793449830867a86922
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2024-05-23 01:15:03 +0000

    fix undefined exception #836

--- src/pyicloud_ipd/base.py
+++ src/pyicloud_ipd/base.py
@@ -14,6 +14,7 @@ import http.cookiejar as cookielib
 import getpass
 
 from pyicloud_ipd.exceptions import (
+    PyiCloudConnectionException,
     PyiCloudFailedLoginException,
     PyiCloudAPIResponseException,
     PyiCloud2SARequiredException,
