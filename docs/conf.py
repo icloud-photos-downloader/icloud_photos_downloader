b4c79979b963b3d14dc62dc2cdb58ccf0e9798ac
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2024-06-16 01:31:28 +0000

    switch to furo theme with myst

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '1.19.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -24,5 +24,5 @@ language = 'Python'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
---
+++ docs/index.md
@@ -0,0 +1,6 @@
+# Welcome to icloudpd's documentation!
+
+```{toctree}
+raw
+size
+```
