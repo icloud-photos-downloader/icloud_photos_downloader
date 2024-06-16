59d318cce86d57d178ec88b0b4c2a467112d8034
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2024-06-16 01:46:10 +0000

    build sphinx cli

--- docs/conf.py
+++ docs/conf.py
@@ -25,4 +25,4 @@ language = 'Python'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_static_path = ['_static']
+# html_static_path = ['_static']
