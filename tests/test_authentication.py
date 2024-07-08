108b928701e6d0298b901cb8c5e97657368b0702
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2024-07-08 20:19:14 +0000

    reading file utf

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -244,7 +244,7 @@ class AuthenticationTestCase(TestCase):
 
     def test_parse_trusted_phone_numbers_payload_valid(self) -> None:
         html_path = os.path.join(self.data_path, "parse_trusted_phone_numbers_payload_valid.html")
-        with open(html_path) as file:
+        with open(html_path, encoding="UTF-8") as file:
             html = file.read()
         expected = _TrustedDevice(id=1, obfuscated_number="(***) ***-**81")
         result = parse_trusted_phone_numbers_payload(html)
