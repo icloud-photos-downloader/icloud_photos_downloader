3656a0a3163dd5391dc9f9e02e4946ca9c4d9dee
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2024-07-10 18:33:11 +0000

    create tzlc report

---
+++ scripts/compile_compatibility.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""takes results of compatibility tests and compie into one file"""
+
+from scripts.compile_matrix import print_breakdowns
+
+if __name__ == "__main__":
+    print("## Minimal Effort Compatibility")
+    print(
+        "Checks if `icloudpd` can be installed using minimal effort and ran bare minimum functionality of displaying a version information. Minimal effort may require installing default version of package manager using OS tools"
+    )
+    print("")
+    print_breakdowns()
---
+++ scripts/compile_tzlc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""takes results of tz and locale compatibility tests and compile into one file"""
+
+from scripts.compile_matrix import print_breakdowns
+
+if __name__ == "__main__":
+    print("## Timezone and Locale Compatibility")
+    print(
+        "Checks if `icloudpd` can be installed using minimal effort and ran bare minimum functionality of displaying version and commit timestamp in local timezone and RU locale. Minimal effort may require installing default version of package manager, timezone data, and locales using OS tools"
+    )
+    print("")
+    print_breakdowns()
