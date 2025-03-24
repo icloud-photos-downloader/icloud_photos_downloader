ea927c49cf60d9b5505b66e9aaf4c8b956b5b196
Andrey Nikiforov <andy.nikiforov@gmail.com>
2025-03-23 18:03:20 -0700

    no century testing on non linux

--- tests/test_download_photos.py
+++ tests/test_download_photos.py
@@ -941,6 +941,8 @@ class DownloadPhotoTestCase(TestCase):
             self.assertIn("INFO     All photos have been downloaded", self._caplog.text)
             assert result.exit_code == 0
 
+    @pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
+    @pytest.mark.skipif(sys.platform == "darwin", reason="does not run on mac")
     def test_creation_date_without_century(self) -> None:
         base_dir = os.path.join(self.fixtures_path, inspect.stack()[0][3])
 
