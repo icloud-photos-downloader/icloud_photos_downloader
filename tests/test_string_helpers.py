09c6cc07d5bbc1ffdc6f59af41b7e0f1bfae4ab2
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2025-06-02 11:31:56 -0700

    extract param parsing (#1147)

--- tests/test_string_helpers.py
+++ tests/test_string_helpers.py
@@ -1,6 +1,12 @@
+import datetime
+import random
+import string
+import sys
 from unittest import TestCase
 
-from icloudpd.string_helpers import truncate_middle
+import pytest
+
+from icloudpd.string_helpers import parse_timestamp_or_timedelta, truncate_middle
 
 
 class TruncateMiddleTestCase(TestCase):
@@ -17,3 +23,78 @@ class TruncateMiddleTestCase(TestCase):
         assert truncate_middle("test_filename.jpg", 0) == ""
         with self.assertRaises(ValueError):
             truncate_middle("test_filename.jpg", -1)
+
+
+class ParseTimestampeOrTimeDeltaTestCase(TestCase):
+    def test_totality(self) -> None:
+        characters = string.ascii_letters + string.digits
+        for _case in range(500):
+            target_length = random.randint(0, 100)
+            target_string = "".join(random.choice(characters) for i in range(target_length))
+            _result = parse_timestamp_or_timedelta(target_string)
+            #  not throwing is okay
+
+    def test_naive(self) -> None:
+        assert parse_timestamp_or_timedelta("2025-01-02T03:04:05.000600") == datetime.datetime(
+            2025, 1, 2, 3, 4, 5, 600
+        )
+        assert parse_timestamp_or_timedelta("2025-01-02T03:04:05.006") == datetime.datetime(
+            2025, 1, 2, 3, 4, 5, 6000
+        )
+        assert parse_timestamp_or_timedelta("2025-01-02") == datetime.datetime(
+            2025, 1, 2, 0, 0, 0, 0
+        )
+
+    @pytest.mark.skipif(
+        sys.version_info < (3, 11), reason="Requires Python 3.11 or higher for full parsing support"
+    )
+    def test_naive_311plus(self) -> None:
+        #  short
+        assert parse_timestamp_or_timedelta("2025-01-02T03:04:05.0006") == datetime.datetime(
+            2025, 1, 2, 3, 4, 5, 600
+        )
+
+    def test_aware(self) -> None:
+        assert parse_timestamp_or_timedelta(
+            "2025-01-02T03:04:05.000600+08:00"
+        ) == datetime.datetime(
+            2025, 1, 2, 3, 4, 5, 600, datetime.timezone(datetime.timedelta(hours=8))
+        )
+        assert parse_timestamp_or_timedelta("2025-01-02T03:04:05.006+08:00") == datetime.datetime(
+            2025, 1, 2, 3, 4, 5, 6000, datetime.timezone(datetime.timedelta(hours=8))
+        )
+
+    @pytest.mark.skipif(
+        sys.version_info < (3, 11), reason="Requires Python 3.11 or higher for full parsing support"
+    )
+    def test_aware_311plus(self) -> None:
+        #  short
+        assert parse_timestamp_or_timedelta("2025-01-02T03:04:05.0006Z") == datetime.datetime(
+            2025, 1, 2, 3, 4, 5, 600, datetime.timezone.utc
+        )
+        assert parse_timestamp_or_timedelta("2025-01-02T03:04:05.0006+08:00") == datetime.datetime(
+            2025, 1, 2, 3, 4, 5, 600, datetime.timezone(datetime.timedelta(hours=8))
+        )
+
+    def test_timestamp_invalid(self) -> None:
+        assert parse_timestamp_or_timedelta("2025-01") is None
+        assert parse_timestamp_or_timedelta("") is None
+
+    def test_delta_totality(self) -> None:
+        digits = string.digits
+        characters = ["d", "D"]
+        for _case in range(500):
+            target_length = random.randint(1, 5)
+            target_string = "".join(
+                random.choice(digits) for i in range(target_length)
+            ) + random.choice(characters)
+            result = parse_timestamp_or_timedelta(target_string)
+            assert isinstance(result, datetime.timedelta)
+
+    def test_delta(self) -> None:
+        assert parse_timestamp_or_timedelta("1d") == datetime.timedelta(days=1)
+        assert parse_timestamp_or_timedelta("0d") == datetime.timedelta(days=0)
+
+    def test_delta_invalid(self) -> None:
+        assert parse_timestamp_or_timedelta("-1d") is None
+        assert parse_timestamp_or_timedelta("") is None
