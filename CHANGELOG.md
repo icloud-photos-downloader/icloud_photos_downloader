8290e6e6088b61716d231bb65008cf4d25e440b0
Andrey Nikiforov <AndreyNikiforov@users.noreply.github.com>
2025-08-17 19:25:04 +0000

    bump 1.29.4 -> 1.30.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Unreleased
 
+## 1.30.0 (2025-08-17)
+
 - feat: resume interrupted downloads [#968](https://github.com/icloud-photos-downloader/icloud_photos_downloader/issues/968) [#793](https://github.com/icloud-photos-downloader/icloud_photos_downloader/issues/793)
 - feat: add `--skip-photos` filter [#401](https://github.com/icloud-photos-downloader/icloud_photos_downloader/issues/401) [#1206](https://github.com/icloud-photos-downloader/icloud_photos_downloader/issues/1206)
 
--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "icloudpd"
 copyright = "2024, Contributors"
 author = "Contributors"
-release = "1.29.4"
+release = "1.30.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -19,7 +19,7 @@ extensions = ["myst_parser"]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_title = "icloudpd 1.29.4"
+html_title = "icloudpd 1.30.0"
 language = "en"
 # language = 'Python'
 
