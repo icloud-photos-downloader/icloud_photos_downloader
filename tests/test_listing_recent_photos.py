14fb992b3b21a546df54bed80ebef276079a3972
Andrey Nikiforov <andy.nikiforov@gmail.com>
2025-08-24 12:35:16 -0700

    remove click dependency

--- tests/test_listing_recent_photos.py
+++ tests/test_listing_recent_photos.py
@@ -1,16 +1,15 @@
-import glob
 import inspect
 import json
 import os
-import shutil
 from unittest import TestCase, mock
 
 import pytest
-from click.testing import CliRunner
 from vcr import VCR
 
-from icloudpd.base import main
-from tests.helpers import path_from_project_root, print_result_exception, recreate_path
+from tests.helpers import (
+    path_from_project_root,
+    run_icloudpd_test,
+)
 
 vcr = VCR(decode_compressed_response=True, record_mode="none")
 
@@ -21,242 +20,162 @@ class ListingRecentPhotosTestCase(TestCase):
         self._caplog = caplog
         self.root_path = path_from_project_root(__file__)
         self.fixtures_path = os.path.join(self.root_path, "fixtures")
-        self.vcr_path = os.path.join(self.root_path, "vcr_cassettes")
 
     def test_listing_recent_photos(self) -> None:
         base_dir = os.path.join(self.fixtures_path, inspect.stack()[0][3])
-        cookie_dir = os.path.join(base_dir, "cookie")
-        data_dir = os.path.join(base_dir, "data")
-        cookie_master_path = os.path.join(self.root_path, "cookie")
+        data_dir, result = run_icloudpd_test(
+            self.assertEqual,
+            self.root_path,
+            base_dir,
+            "listing_photos.yml",
+            [],
+            [],
+            [
+                "--username",
+                "jdoe@gmail.com",
+                "--password",
+                "password1",
+                "--recent",
+                "5",
+                "--only-print-filenames",
+                "--no-progress-bar",
+                "--threads-num",
+                "1",
+            ],
+        )
+        self.assertEqual(result.exit_code, 0, "exit code")
 
-        for dir in [base_dir, data_dir]:
-            recreate_path(dir)
+        filenames = result.output.splitlines()
 
-        shutil.copytree(cookie_master_path, cookie_dir)
-
-        # Note - This test uses the same cassette as test_download_photos.py
-        with vcr.use_cassette(os.path.join(self.vcr_path, "listing_photos.yml")):
-            # Pass fixed client ID via environment variable
-            runner = CliRunner(env={"CLIENT_ID": "DE309E26-942E-11E8-92F5-14109FE0B321"})
-            result = runner.invoke(
-                main,
-                [
-                    "--username",
-                    "jdoe@gmail.com",
-                    "--password",
-                    "password1",
-                    "--recent",
-                    "5",
-                    "--only-print-filenames",
-                    "--no-progress-bar",
-                    "--threads-num",
-                    "1",
-                    "-d",
-                    data_dir,
-                    "--cookie-directory",
-                    cookie_dir,
-                ],
-            )
-            print_result_exception(result)
-            filenames = result.output.splitlines()
-
-            self.assertEqual(len(filenames), 8)
-            self.assertIn(
-                os.path.join(data_dir, os.path.normpath("2018/07/31/IMG_7409.JPG")), filenames[0]
-            )
-            self.assertEqual(
-                os.path.join(data_dir, os.path.normpath("2018/07/31/IMG_7409.MOV")), filenames[1]
-            )
-            self.assertEqual(
-                os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7408.JPG")), filenames[2]
-            )
-            self.assertEqual(
-                os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7408.MOV")), filenames[3]
-            )
-            self.assertEqual(
-                os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7407.JPG")), filenames[4]
-            )
-            self.assertEqual(
-                os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7407.MOV")), filenames[5]
-            )
-            self.assertEqual(
-                os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7405.MOV")), filenames[6]
-            )
-            self.assertEqual(
-                os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7404.MOV")), filenames[7]
-            )
-
-            assert result.exit_code == 0
-
-        files_in_result = glob.glob(os.path.join(data_dir, "**/*.*"), recursive=True)
-
-        assert sum(1 for _ in files_in_result) == 0
+        self.assertEqual(len(filenames), 8)
+        self.assertIn(
+            os.path.join(data_dir, os.path.normpath("2018/07/31/IMG_7409.JPG")), filenames[0]
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/31/IMG_7409.MOV")), filenames[1]
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7408.JPG")), filenames[2]
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7408.MOV")), filenames[3]
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7407.JPG")), filenames[4]
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7407.MOV")), filenames[5]
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7405.MOV")), filenames[6]
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7404.MOV")), filenames[7]
+        )
 
     def test_listing_photos_does_not_create_folders(self) -> None:
         base_dir = os.path.join(self.fixtures_path, inspect.stack()[0][3])
-        cookie_dir = os.path.join(base_dir, "cookie")
-        data_dir = os.path.join(base_dir, "data")
-        cookie_master_path = os.path.join(self.root_path, "cookie")
 
-        for dir in [base_dir, data_dir]:
-            recreate_path(dir)
-
-        shutil.copytree(cookie_master_path, cookie_dir)
-
-        # make sure the directory does not exist yet.
+        data_dir, result = run_icloudpd_test(
+            self.assertEqual,
+            self.root_path,
+            base_dir,
+            "listing_photos.yml",
+            [],
+            [],
+            [
+                "--username",
+                "jdoe@gmail.com",
+                "--password",
+                "password1",
+                "--recent",
+                "5",
+                "--only-print-filenames",
+                "--no-progress-bar",
+                "--threads-num",
+                "1",
+            ],
+        )
+        self.assertEqual(result.exit_code, 0, "exit code")
         # Should only be created after download, not after just --print-filenames
         self.assertFalse(os.path.exists(os.path.join(data_dir, os.path.normpath("2018/07/31"))))
 
-        # Note - This test uses the same cassette as test_download_photos.py
-        with vcr.use_cassette(os.path.join(self.vcr_path, "listing_photos.yml")):
-            # Pass fixed client ID via environment variable
-            runner = CliRunner(env={"CLIENT_ID": "DE309E26-942E-11E8-92F5-14109FE0B321"})
-            result = runner.invoke(
-                main,
-                [
-                    "--username",
-                    "jdoe@gmail.com",
-                    "--password",
-                    "password1",
-                    "--recent",
-                    "5",
-                    "--only-print-filenames",
-                    "--no-progress-bar",
-                    "--threads-num",
-                    "1",
-                    "-d",
-                    data_dir,
-                    "--cookie-directory",
-                    cookie_dir,
-                ],
-            )
-            print_result_exception(result)
-            # make sure the directory still does not exist.
-            # Should only be created after download, not after just --print-filenames
-            self.assertFalse(os.path.exists(os.path.join(data_dir, os.path.normpath("2018/07/31"))))
-
-            assert result.exit_code == 0
-
-        files_in_result = glob.glob(os.path.join(data_dir, "**/*.*"), recursive=True)
-
-        assert sum(1 for _ in files_in_result) == 0
-
     def test_listing_recent_photos_with_missing_filenameEnc(self) -> None:
         base_dir = os.path.join(self.fixtures_path, inspect.stack()[0][3])
-        cookie_dir = os.path.join(base_dir, "cookie")
-        data_dir = os.path.join(base_dir, "data")
-        cookie_master_path = os.path.join(self.root_path, "cookie")
 
-        for dir in [base_dir, data_dir]:
-            recreate_path(dir)
+        data_dir, result = run_icloudpd_test(
+            self.assertEqual,
+            self.root_path,
+            base_dir,
+            "listing_photos_missing_filenameEnc.yml",
+            [],
+            [],
+            [
+                "--username",
+                "jdoe@gmail.com",
+                "--password",
+                "password1",
+                "--recent",
+                "5",
+                "--only-print-filenames",
+                "--no-progress-bar",
+            ],
+        )
+        self.assertEqual(result.exit_code, 0, "exit code")
+        filenames = result.output.splitlines()
 
-        shutil.copytree(cookie_master_path, cookie_dir)
-
-        # Note - This test uses the same cassette as test_download_photos.py
-        with vcr.use_cassette(  # noqa: SIM117
-            os.path.join(self.vcr_path, "listing_photos_missing_filenameEnc.yml")
-        ):
-            with mock.patch("icloudpd.base.open", create=True):
-                with mock.patch.object(json, "dump"):
-                    # Pass fixed client ID via environment variable
-                    runner = CliRunner(env={"CLIENT_ID": "DE309E26-942E-11E8-92F5-14109FE0B321"})
-                    result = runner.invoke(
-                        main,
-                        [
-                            "--username",
-                            "jdoe@gmail.com",
-                            "--password",
-                            "password1",
-                            "--recent",
-                            "5",
-                            "--only-print-filenames",
-                            "--no-progress-bar",
-                            "--threads-num",
-                            "1",
-                            "-d",
-                            data_dir,
-                            "--cookie-directory",
-                            cookie_dir,
-                        ],
-                    )
-                    print_result_exception(result)
-
-                    self.assertEqual.__self__.maxDiff = None  # type: ignore[attr-defined]
-
-                    filenames = result.output.splitlines()
-
-                    # self.assertEqual(len(filenames), 5)
-                    self.assertEqual(
-                        os.path.join(data_dir, os.path.normpath("2018/07/31/AY6c_BsE0jja.JPG")),
-                        filenames[0],
-                    )
-                    self.assertEqual(
-                        os.path.join(data_dir, os.path.normpath("2018/07/31/AY6c_BsE0jja.MOV")),
-                        filenames[1],
-                    )
-                    self.assertEqual(
-                        os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7408.JPG")),
-                        filenames[2],
-                    )
-                    self.assertEqual(
-                        os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7408.MOV")),
-                        filenames[3],
-                    )
-                    self.assertEqual(
-                        os.path.join(data_dir, os.path.normpath("2018/07/30/AZ_wAGT9P6jh.JPG")),
-                        filenames[4],
-                    )
-                    assert result.exit_code == 0
-
-        files_in_result = glob.glob(os.path.join(data_dir, "**/*.*"), recursive=True)
-
-        assert sum(1 for _ in files_in_result) == 0
+        # self.assertEqual(len(filenames), 5)
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/31/AY6c_BsE0jja.JPG")),
+            filenames[0],
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/31/AY6c_BsE0jja.MOV")),
+            filenames[1],
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7408.JPG")),
+            filenames[2],
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/30/IMG_7408.MOV")),
+            filenames[3],
+        )
+        self.assertEqual(
+            os.path.join(data_dir, os.path.normpath("2018/07/30/AZ_wAGT9P6jh.JPG")),
+            filenames[4],
+        )
 
     # This was used to solve the missing filenameEnc error. I found
     # another case where it might crash. (Maybe Apple changes the downloadURL key)
     def test_listing_recent_photos_with_missing_downloadURL(self) -> None:
         base_dir = os.path.join(self.fixtures_path, inspect.stack()[0][3])
-        cookie_dir = os.path.join(base_dir, "cookie")
-        data_dir = os.path.join(base_dir, "data")
-        cookie_master_path = os.path.join(self.root_path, "cookie")
-
-        for dir in [base_dir, data_dir]:
-            recreate_path(dir)
-
-        shutil.copytree(cookie_master_path, cookie_dir)
-
-        # Note - This test uses the same cassette as test_download_photos.py
-        with vcr.use_cassette(  # noqa: SIM117
-            os.path.join(self.vcr_path, "listing_photos_missing_downloadUrl.yml")
+        with (
+            mock.patch("icloudpd.base.open", create=True) as mock_open,
+            mock.patch.object(json, "dump") as mock_json,
         ):
-            with mock.patch("icloudpd.base.open", create=True) as mock_open:
-                with mock.patch.object(json, "dump") as mock_json:
-                    # Pass fixed client ID via environment variable
-                    runner = CliRunner(env={"CLIENT_ID": "DE309E26-942E-11E8-92F5-14109FE0B321"})
-                    result = runner.invoke(
-                        main,
-                        [
-                            "--username",
-                            "jdoe@gmail.com",
-                            "--password",
-                            "password1",
-                            "--recent",
-                            "1",
-                            "--only-print-filenames",
-                            "--no-progress-bar",
-                            "--threads-num",
-                            "1",
-                            "-d",
-                            data_dir,
-                            "--cookie-directory",
-                            cookie_dir,
-                        ],
-                    )
-                    print_result_exception(result)
-
-                    self.assertEqual.__self__.maxDiff = None  # type: ignore[attr-defined]
-                    self.assertEqual(
-                        """\
+            _, result = run_icloudpd_test(
+                self.assertEqual,
+                self.root_path,
+                base_dir,
+                "listing_photos_missing_downloadUrl.yml",
+                [],
+                [],
+                [
+                    "--username",
+                    "jdoe@gmail.com",
+                    "--password",
+                    "password1",
+                    "--recent",
+                    "1",
+                    "--only-print-filenames",
+                    "--no-progress-bar",
+                ],
+            )
+            self.assertEqual(result.exit_code, 0, "exit code")
+            self.assertEqual.__self__.maxDiff = None  # type: ignore[attr-defined]
+            self.assertEqual(
+                """\
 KeyError: 'downloadURL' attribute was not found in the photo fields.
 icloudpd has saved the photo record to: ./icloudpd-photo-error.json
 Please create a Gist with the contents of this file: https://gist.github.com
@@ -264,30 +183,25 @@ Then create an issue on GitHub: https://github.com/icloud-photos-downloader/iclo
 Include a link to the Gist in your issue, so that we can see what went wrong.
 
 """,
-                        result.output,
-                    )
-                    mock_open.assert_called_once_with(
-                        file="icloudpd-photo-error.json", mode="w", encoding="utf8"
-                    )
-                    mock_json.assert_called()
-                    self.assertEqual(len(mock_json.call_args_list), 7)
-                    # Check a few keys in the dict
-                    first_arg = mock_json.call_args_list[6][0][0]
-                    self.assertEqual(
-                        first_arg["master_record"]["recordName"], "AY6c+BsE0jjaXx9tmVGJM1D2VcEO"
-                    )
-                    self.assertEqual(
-                        first_arg["master_record"]["fields"]["resVidSmallHeight"]["value"], 581
-                    )
-                    self.assertEqual(
-                        first_arg["asset_record"]["recordName"],
-                        "F2A23C38-0020-42FE-A273-2923ADE3CAED",
-                    )
-                    self.assertEqual(
-                        first_arg["asset_record"]["fields"]["assetDate"]["value"], 1533021744816
-                    )
-                    assert result.exit_code == 0
-
-        files_in_result = glob.glob(os.path.join(data_dir, "**/*.*"), recursive=True)
-
-        assert sum(1 for _ in files_in_result) == 0
+                result.output,
+            )
+            mock_open.assert_called_once_with(
+                file="icloudpd-photo-error.json", mode="w", encoding="utf8"
+            )
+            mock_json.assert_called()
+            self.assertEqual(len(mock_json.call_args_list), 7)
+            # Check a few keys in the dict
+            first_arg = mock_json.call_args_list[6][0][0]
+            self.assertEqual(
+                first_arg["master_record"]["recordName"], "AY6c+BsE0jjaXx9tmVGJM1D2VcEO"
+            )
+            self.assertEqual(
+                first_arg["master_record"]["fields"]["resVidSmallHeight"]["value"], 581
+            )
+            self.assertEqual(
+                first_arg["asset_record"]["recordName"],
+                "F2A23C38-0020-42FE-A273-2923ADE3CAED",
+            )
+            self.assertEqual(
+                first_arg["asset_record"]["fields"]["assetDate"]["value"], 1533021744816
+            )
