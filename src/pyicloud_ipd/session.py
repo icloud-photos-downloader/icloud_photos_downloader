7e7e11782c585aaca2126066fd577dba758d6d35
Andrey Nikiforov <andy.nikiforov@gmail.com>
2025-09-12 01:31:20 -0700

    Revert "move response processing out of session"
    This reverts commit c82491d3a2ce1fd5505229f719392b82300496ec.

--- src/pyicloud_ipd/session.py
+++ src/pyicloud_ipd/session.py
@@ -1,12 +1,13 @@
 import inspect
 import json
 import logging
-from typing import Any, Callable, Mapping, NoReturn
+import typing
+from typing import Any, Callable, Dict, Mapping, NoReturn, Sequence
 
 from requests import Response, Session
 from typing_extensions import override
 
-from foundation.http import response_to_har_entry
+from foundation.http import is_streaming_response, response_to_har_entry
 from pyicloud_ipd.exceptions import (
     PyiCloud2SARequiredException,
     PyiCloudAPIResponseException,
@@ -73,10 +74,12 @@ class PyiCloudSession(Session):
 
         if "timeout" not in kwargs and self.service.http_timeout is not None:
             kwargs["timeout"] = self.service.http_timeout
-
         response = throw_on_503(
             self.observe(handle_connection_error(super().request)(method, url, **kwargs))
         )
+
+        content_type = response.headers.get("Content-Type", "").split(";")[0]
+        json_mimetypes = ["application/json", "text/json"]
 
         request_logger.debug(response.headers)
 
@@ -94,62 +97,59 @@ class PyiCloudSession(Session):
         self.cookies.save(ignore_discard=True, ignore_expires=True)  # type: ignore[attr-defined]
         LOGGER.debug("Cookies saved to %s", self.service.cookiejar_path)
 
-        # content_type = response.headers.get("Content-Type", "").split(";")[0]
-        # json_mimetypes = ["application/json", "text/json"]
+        if (
+            not response.ok
+            and (content_type not in json_mimetypes or response.status_code in [421, 450, 500])
+            and not is_streaming_response(response)
+        ):
+            self._raise_error(str(response.status_code), response.reason)
 
-        # if (
-        #     not response.ok
-        #     and content_type not in json_mimetypes
-        #     and not is_streaming_response(response)
-        # ):
-        #     self._raise_error(str(response.status_code), response.reason)
+        if content_type not in json_mimetypes:
+            if self.service.session_data.get("apple_rscd") == "401":
+                code: str | None = "401"
+                reason: str | None = "Invalid username/password combination."
+                self._raise_error(code or "Unknown", reason or "Unknown")
 
-        # if content_type not in json_mimetypes:
-        #     if self.service.session_data.get("apple_rscd") == "401":
-        #         code: str | None = "401"
-        #         reason: str | None = "Invalid username/password combination."
-        #         self._raise_error(code or "Unknown", reason or "Unknown")
+            return response
 
-        #     return response
+        try:
+            data = response.json() if response.status_code != 204 else {}
+        except ValueError:
+            request_logger.warning("Failed to parse response with JSON mimetype")
+            return response
 
-        # try:
-        #     data = response.json() if response.status_code != 204 else {}
-        # except ValueError:
-        #     request_logger.warning("Failed to parse response with JSON mimetype")
-        #     return response
+        request_logger.debug(data)
 
-        # request_logger.debug(data)
+        if isinstance(data, dict):
+            if data.get("hasError"):
+                errors: Sequence[Dict[str, Any]] | None = typing.cast(
+                    Sequence[Dict[str, Any]] | None, data.get("service_errors")
+                )
+                # service_errors returns a list of dict
+                #    dict includes the keys: code, title, message, supressDismissal
+                # Assuming a single error for now
+                # May need to revisit to capture and handle multiple errors
+                if errors:
+                    code = errors[0].get("code")
+                    reason = errors[0].get("message")
+                self._raise_error(code or "Unknown", reason or "Unknown")
+            elif not data.get("success"):
+                reason = data.get("errorMessage")
+                reason = reason or data.get("reason")
+                reason = reason or data.get("errorReason")
+                if not reason and isinstance(data.get("error"), str):
+                    reason = data.get("error")
+                if not reason and data.get("error"):
+                    reason = "Unknown reason"
 
-        # if isinstance(data, dict):
-        #     if data.get("hasError"):
-        #         errors: Sequence[Dict[str, Any]] | None = typing.cast(
-        #             Sequence[Dict[str, Any]] | None, data.get("service_errors")
-        #         )
-        #         # service_errors returns a list of dict
-        #         #    dict includes the keys: code, title, message, supressDismissal
-        #         # Assuming a single error for now
-        #         # May need to revisit to capture and handle multiple errors
-        #         if errors:
-        #             code = errors[0].get("code")
-        #             reason = errors[0].get("message")
-        #         self._raise_error(code or "Unknown", reason or "Unknown")
-        #     elif not data.get("success"):
-        #         reason = data.get("errorMessage")
-        #         reason = reason or data.get("reason")
-        #         reason = reason or data.get("errorReason")
-        #         if not reason and isinstance(data.get("error"), str):
-        #             reason = data.get("error")
-        #         if not reason and data.get("error"):
-        #             reason = "Unknown reason"
+                code = data.get("errorCode")
+                if not code and data.get("serverErrorCode"):
+                    code = data.get("serverErrorCode")
+                if not code and data.get("error"):
+                    code = data.get("error")
 
-        #         code = data.get("errorCode")
-        #         if not code and data.get("serverErrorCode"):
-        #             code = data.get("serverErrorCode")
-        #         if not code and data.get("error"):
-        #             code = data.get("error")
-
-        #         if reason:
-        #             self._raise_error(code or "Unknown", reason)
+                if reason:
+                    self._raise_error(code or "Unknown", reason)
 
         return response
 
