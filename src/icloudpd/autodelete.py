79af429bdb29e7c500e6ebfd7f1fb87a19cf1f80
Sebastián Maluk <samaluk@miuandes.cl>
2026-06-12 01:34:55 -0400

    refactor: add local download path planner

--- src/icloudpd/autodelete.py
+++ src/icloudpd/autodelete.py
@@ -11,12 +11,12 @@ from typing import Callable, Sequence, Set
 from tzlocal import get_localzone
 
 from icloudpd.paths import local_download_path
-from pyicloud_ipd.asset_version import calculate_version_filename
+from pyicloud_ipd.asset_version import add_suffix_to_filename, calculate_version_filename
 from pyicloud_ipd.file_match import FileMatchPolicy
 from pyicloud_ipd.live_photo_mov_filename_policy import LivePhotoMovFilenamePolicy
 from pyicloud_ipd.raw_policy import RawTreatmentPolicy
 from pyicloud_ipd.services.photos import PhotoAsset, PhotoLibrary
-from pyicloud_ipd.utils import disambiguate_filenames
+from pyicloud_ipd.utils import disambiguate_filenames, size_to_suffix
 from pyicloud_ipd.version_size import AssetVersionSize, LivePhotoVersionSize, VersionSize
 
 
@@ -76,6 +76,98 @@ def _download_dir_for_media(
             date_path = folder_structure.format(created_date)
 
     return os.path.normpath(os.path.join(directory, date_path))
+
+
+def _add_path_variants(paths: Set[str], path: str, version_size: int) -> None:
+    normalized_path = os.path.normpath(path)
+    paths.add(normalized_path)
+    paths.add((f"-{version_size}.").join(normalized_path.rsplit(".", 1)))
+
+
+def local_download_paths_for_media(
+    logger: logging.Logger,
+    media: PhotoAsset,
+    config: LocalDownloadPathConfig,
+    include_all_versions: bool = False,
+) -> Set[str]:
+    download_dir = _download_dir_for_media(logger, config.folder_structure, config.directory, media)
+    paths: Set[str] = set()
+    raw_versions = media.versions_with_raw_policy(config.raw_policy)
+    versions, filename_overrides = disambiguate_filenames(
+        raw_versions,
+        config.sizes,
+        media,
+        config.lp_filename_generator,
+    )
+
+    for _size in config.sizes:
+        if _size not in versions and _size != AssetVersionSize.ORIGINAL:
+            if config.force_size:
+                continue
+            if AssetVersionSize.ORIGINAL in config.sizes:
+                continue
+            _size = AssetVersionSize.ORIGINAL
+
+        version = versions[_size]
+        filename = calculate_version_filename(
+            media.filename,
+            version,
+            _size,
+            config.lp_filename_generator,
+            media.item_type,
+            filename_overrides.get(_size),
+        )
+        download_path = local_download_path(filename, download_dir)
+        _add_path_variants(paths, download_path, version.size)
+        if _size == AssetVersionSize.ORIGINAL:
+            _add_path_variants(
+                paths, add_suffix_to_filename("-original", download_path), version.size
+            )
+
+        if config.xmp_sidecar:
+            paths.add(os.path.normpath(download_path) + ".xmp")
+
+    if include_all_versions:
+        _size: VersionSize
+        for _size, version in raw_versions.items():
+            if _size in [AssetVersionSize.ALTERNATIVE, AssetVersionSize.ADJUSTED]:
+                continue
+            filename = calculate_version_filename(
+                media.filename,
+                version,
+                _size,
+                config.lp_filename_generator,
+                media.item_type,
+            )
+            download_path = local_download_path(filename, download_dir)
+            _add_path_variants(paths, download_path, version.size)
+            if config.xmp_sidecar:
+                paths.add(os.path.normpath(download_path) + ".xmp")
+
+    if not config.skip_live_photos and config.live_photo_size in raw_versions:
+        version = raw_versions[config.live_photo_size]
+        lp_filename = calculate_version_filename(
+            media.filename,
+            version,
+            config.live_photo_size,
+            config.lp_filename_generator,
+            media.item_type,
+        )
+        if config.live_photo_size != LivePhotoVersionSize.ORIGINAL:
+            lp_filename = add_suffix_to_filename(
+                size_to_suffix(config.live_photo_size), lp_filename
+            )
+        _add_path_variants(paths, os.path.join(download_dir, lp_filename), version.size)
+
+    if config.file_match_policy != FileMatchPolicy.NAME_SIZE_DEDUP_WITH_SUFFIX:
+        paths = {path for path in paths if not _is_size_dedup_path(path)}
+
+    return paths
+
+
+def _is_size_dedup_path(path: str) -> bool:
+    stem, extension = os.path.splitext(path)
+    return extension != "" and stem.rsplit("-", 1)[-1].isdigit()
 
 
 def autodelete_photos(
